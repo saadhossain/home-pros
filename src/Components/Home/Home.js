a2f9c16e25e05a915455030877cad3abefa96a7a
saadhossain <mshsarkar@gmail.com>
2022-10-24 15:19:56 +0600

    Clicent testimonials added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,6 +5,7 @@ import FeatureVideo from './FeatureVideo';
 import HomeCarousel from './HomeCarousel';
 import HomeProperty from './HomeProperty';
 import PropertySearch from './PropertySearch';
+import Testimonials from './Testimonials';
 const Home = () => {
     const properties = useContext(DataContext)
     return (
@@ -12,6 +13,7 @@ const Home = () => {
             {/* Home Carousel */}
             <HomeCarousel></HomeCarousel>
             <PropertySearch></PropertySearch>
+            <h2 className='text-2xl md:text-6xl font-bold text-hpro2nd text-center my-5'>Our Top Featured Property</h2>
             <div className='w-10/12 mx-auto my-8 grid md:grid-cols-3 gap-5'>
                 {
                     properties.map(property => <HomeProperty key={property.id} property={property}></HomeProperty>)
@@ -19,6 +21,8 @@ const Home = () => {
             </div>
             <FeatureVideo></FeatureVideo>
             <AboutUsSection></AboutUsSection>
+            <h2 className='text-2xl md:text-6xl font-bold text-hpro2nd text-center my-5'>Clients Testimonials</h2>
+            <Testimonials></Testimonials>
         </div>
     );
 };
---
+++ src/Components/Home/Testimonials.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import person1 from '../../assests/testimonials/1.png';
+import person2 from '../../assests/testimonials/2.png';
+import person3 from '../../assests/testimonials/3.png';
+import person4 from '../../assests/testimonials/4.png';
+import person5 from '../../assests/testimonials/5.png';
+
+
+const Testimonials = () => {
+    return (
+        <div className='w-10/12 mx-auto relative'>
+            <div className="carousel carousel-center p-4 space-x-4 rounded-box">
+                {/* Testimonial Items 1 */}
+                <div className="carousel-item w-11/12 md:w-1/4 flex-col bg-gray-100 p-5 rounded-lg shadow-lg" id='item1'>
+                    <p>
+                        Home Pros is an outstanding agent to work with. They are skilled listener and negotiator, with very in-depth knowledge of the local markets. My husband and I listed two properties with them at the same time and while they sold one within a week.
+                    </p>
+                    <div className='flex gap-2 items-center my-5'>
+                        <img src={person1} alt='' className='w-20' />
+                        <h2 className='text-lg font-bold'>Abagael Long</h2>
+                    </div>
+                </div>
+                {/* Testimonial Items 2 */}
+                <div className="carousel-item w-11/12 md:w-1/4 flex-col bg-gray-100 p-5 rounded-lg shadow-lg" id='item2'>
+                    <p>
+                    We recently purchased a condominium in New Jersey with Home Pros working as our buyer's agent. We were searching for a perfect place to live. From start to finish David ushered us through the process in a very thorough and gracious manner.
+                    </p>
+                    <div className='flex gap-2 items-center my-5'>
+                        <img src={person2} alt='' className='w-20' />
+                        <h2 className='text-lg font-bold'>Robert Morrill</h2>
+                    </div>
+                </div>
+                {/* Testimonial Items 3 */}
+                <div className="carousel-item w-11/12 md:w-1/4 flex-col bg-gray-100 p-5 rounded-lg shadow-lg" id='item3'>
+                    <p>
+                        My wife and I had a dream of downsizing from our house in Cape Elizabeth into a small condo closer to where we work and play in Portland. Home Pros and their skilled team helped make that dream a reality.The sale went smoothly.
+                    </p>
+                    <div className='flex gap-2 items-center my-5'>
+                        <img src={person3} alt='' className='w-20' />
+                        <h2 className='text-lg font-bold'>Shawn Baldwin</h2>
+                    </div>
+                </div>
+                {/* Testimonial Items 4 */}
+                <div className="carousel-item w-11/12 md:w-1/4 flex-col bg-gray-100 p-5 rounded-lg shadow-lg" id='item4'>
+                    <p>
+                        I recently sold a house with Home Pros and while this can be a very stressful process, I felt 110% confident by partnering with Them. They was candid, provided great feedback, helped explain clearly all details and managed the actual sale negotiation.
+                    </p>
+                    <div className='flex gap-2 items-center my-5'>
+                        <img src={person4} alt='' className='w-20' />
+                        <h2 className='text-lg font-bold'>Hanna Chen</h2>
+                    </div>
+                </div>
+                {/* Testimonial Items 5 */}
+                <div className="carousel-item w-11/12 md:w-1/4 flex-col bg-gray-100 p-5 rounded-lg shadow-lg" id='item5'>
+                    <p>
+                        I would highly recommend Home Pros. They gave us many helpful suggestions on how to update our home. We followed many of his ideas and our home looked amazing. Home Pros is very professional and did a very in depth market analysis.
+                    </p>
+                    <div className='flex gap-2 items-center my-5'>
+                        <img src={person5} alt='' className='w-20' />
+                        <h2 className='text-lg font-bold'>Robert Morrill</h2>
+                    </div>
+                </div>
+
+            </div>
+            <div className="flex justify-between w-full py-2 gap-2 text-white absolute top-[40%]">
+                <a href="#item1" className="bg-hpro2nd p-2 rounded-full"><IoIosArrowBack></IoIosArrowBack></a>
+                <a href="#item5" className="bg-hpro2nd p-2 rounded-full"><IoIosArrowForward></IoIosArrowForward></a>
+            </div>
+        </div>
+    );
+};
+
+export default Testimonials;
