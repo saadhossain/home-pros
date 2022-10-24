0b0840ea86341c41c4e0a5cdc04df4d87234a30d
saadhossain <mshsarkar@gmail.com>
2022-10-24 19:30:28 +0600

    Firebase added and login register form added

--- src/Routers/Routes.js
+++ src/Routers/Routes.js
@@ -2,14 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../Components/Home/Home";
 import Main from "../Layout/Main";
 import PropertyLoader from "../Loader/PropertyLoader";
-
+import Login from '../Components/Login/Login'
+import Register from "../Components/Register/Register";
 export const routers = createBrowserRouter([
     {
         path: '/',
         loader:PropertyLoader,
         element:<Main></Main>,
         children:[
-            {path:'/', element:<Home></Home>}
-        ]
+            {path:'/', element:<Home></Home>},
+            {path:'/login', element:<Login></Login>},
+            {path:'/register', element:<Register></Register>},
+        ],
     }
 ])
